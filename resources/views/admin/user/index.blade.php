c64ef147b0dbece0eab7b2ed99971d773175679d
Watchara Ketkaew <wk996110@gmail.com>
2023-08-09 14:27:32 +0700

    อัพเดทหลังบ้านครั้งที่ 2

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.masterbackend')
+@section('text')
+
+
+@endsection
--- resources/views/layouts/masterbackend.blade.php
+++ resources/views/layouts/masterbackend.blade.php
@@ -25,8 +25,8 @@
       <!-- partial:partials/_navbar.html -->
       <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-          <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('backend/assets/images/logo.png')}}" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('backend/assets/images/logo.png')}}" alt="logo" /></a>
+          <a class="navbar-brand brand-logo" href="{{ route('dashboard') }}"><img src="{{ asset('backend/assets/images/logo.png')}}" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}"><img src="{{ asset('backend/assets/images/logo.png')}}" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-stretch">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -96,13 +96,13 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="index.html">
+              <a class="nav-link" href="">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="index.html">
+              <a class="nav-link" href="{{ route('u.index') }}">
                 <span class="menu-title">User</span>
                 <i class="mdi mdi-account menu-icon"></i>
               </a>
